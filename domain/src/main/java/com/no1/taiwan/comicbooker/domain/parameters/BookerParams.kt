ca9445ba6299eb7fc7d12f356f71ae21f3d409b4
Jieyi <pokkbaby@gmail.com>
2018-09-18 18:58:45 +0900

    Feature: added the parameter for requesting an api.

---
+++ domain/src/main/java/com/no1/taiwan/comicbooker/domain/parameters/BookerParams.kt
@@ -0,0 +1,22 @@
+package com.no1.taiwan.comicbooker.domain.parameters
+
+import com.no1.taiwan.comicbooker.ext.const.DEFAULT_LONG
+import com.no1.taiwan.comicbooker.ext.const.DEFAULT_STR
+import com.no1.taiwan.comicbooker.ext.const.UniqueId
+
+data class BookerParams(
+    val id: UniqueId = DEFAULT_LONG,
+    val desc: String = DEFAULT_STR
+) : Parameterable {
+    companion object {
+        const val PARAM_ID = "id"
+        const val PARAM_NAME = "name"
+    }
+
+    override fun toParameter() = hashMapOf(
+        PARAM_ID to id.toString(),
+        PARAM_NAME to desc
+    )
+
+    override fun toAnyParameter() = throw UnsupportedOperationException()
+}
--- domain/src/main/java/com/no1/taiwan/comicbooker/domain/usecases/TestUsecase.kt
+++ domain/src/main/java/com/no1/taiwan/comicbooker/domain/usecases/TestUsecase.kt
@@ -1,13 +1,16 @@
 package com.no1.taiwan.comicbooker.domain.usecases
 
 import com.no1.taiwan.comicbooker.domain.DeferredUsecase
+import com.no1.taiwan.comicbooker.domain.parameters.BookerParams
 import com.no1.taiwan.comicbooker.domain.repositories.DataRepository
 import com.no1.taiwan.comicbooker.domain.usecases.TestUsecase.Request
 
 class TestUsecase(
     private val repository: DataRepository
 ) : DeferredUsecase<String, Request>() {
-    override suspend fun fetchCase() = repository.fetchTest().await()
+    override suspend fun fetchCase() = requireNotNull(requestValues?.run {
+        repository.fetchTest().await()
+    })
 
-    class Request : RequestValues
+    class Request(val parameters: BookerParams = BookerParams()) : RequestValues
 }
